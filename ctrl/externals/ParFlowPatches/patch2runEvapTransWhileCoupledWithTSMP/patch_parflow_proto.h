87259892054043a3b10492751600720ae5f60098
Niklas Wagner <n.wagner@fz-juelich.de>
2021-03-15 15:47:24 +0100

    add used source-code changes as patchfile (solve #7)

---
+++ ctrl/externals/ParFlowPatches/patch2runEvapTransWhileCoupledWithTSMP/patch_parflow_proto.h
@@ -0,0 +1,10 @@
+--- /p/scratch/cjibg35/tsmpforecast/development/tmp/TSMP/parflow3_2/pfsimulator/parflow_lib/parflow_proto.h	2021-03-15 11:57:20.754055885 +0100
++++ ./pfsimulator/parflow_lib/parflow_proto.h	2021-02-15 11:37:22.344624664 +0100
+@@ -1081,6 +1081,7 @@
+ 		     double stop_time,       /* Stopping time */
+ 		     PFModule *time_step_control, /* Use this module to control timestep if supplied */
+ 		     Vector *evap_trans,     /* Flux from land surface model */ 
++		     Vector *irrigation_sk,     /* Irrigation from evap_trans file*/ 
+ 		     Vector **pressure_out,  /* Output vars */
+ 		     Vector **porosity_out,
+ 		     Vector **saturation_out
