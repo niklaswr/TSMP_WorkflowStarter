462b37238a387256d49c653cafcff8d85e8d06f7
Niklas Wagner <n.wagner@fz-juelich.de>
2023-01-17 15:58:41 +0100

    ADD new INT2LM scripts
    - adjusted int2lm script to new export_path structure etc.

--- ctrl/INT2LM_submit.sh
+++ ctrl/INT2LM_submit.sh
@@ -1,20 +1,19 @@
-#!/bin/bash
+#!/bin/bash -x
 #
 #SBATCH --job-name="int2lm"
 #SBATCH --nodes=1
-#SBATCH --ntasks=48
-#SBATCH --ntasks-per-node=48
+#SBATCH --ntasks=128
+#SBATCH --ntasks-per-node=128
 #SBATCH --output=int2lm.out
 #SBATCH --error=int2lm.err
-#SBATCH --time=03:00:00
-#SBATCH --partition=esm
+#SBATCH --time=01:59:00
+#SBATCH --partition=dc-cpu-devel
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=n.wagner@fz-juelich.de
-#SBATCH --account=esmtst
+#SBATCH --account=jjsc39
 #
-# author: Liuba POSHYVAILO, Niklas WAGNER
-# e-mail: l.poshyvailo@fz-juelich.de, n.wagner@fz-juelich.de
-# last modified: 2022-01-11
+# Owner / author: Niklas WAGNER, n.wagner@fz-juelich.de;
+#                 Liuba POSHYVAILO, l.poshyvailo@fz-juelich.de
 # USAGE: 
 # >> sbatch $0 CTRLDIR
 # >> sbatch INT2LM_submit.sh $(pwd)
@@ -22,14 +21,18 @@
 ###############################################################################
 #### Adjust according to your need BELOW
 ###############################################################################
-startDate=20090101  # start date
-int2lm_hstop=2976   # --> 366 * 24h
-int2lm_hincbound=3  # dumpinterval of forcing (3h, 6h, daily, etc)
+startDate=20200701  # start date
+int2lm_hstop=744    # --> 31 * 24h
+int2lm_hincbound=3  # dumpinterval of forcing (3h, 6h, 24h, etc)
 dependency=3949924  # JOBID to depend the following jobs at
                     # if set JOBID is below latest JOBID the job starts without
                     # dependency automatically
 CTRLDIR=$1          # assuming one is executing this script from the
                     # BASE_CTRLDIR, what is the cast most of the time
+# PROC (processor) distribution for int2lm
+PROCX_INT2LM=16
+PROCY_INT2LM=8
+PROCIO_INT2LM=0
 ###############################################################################
 #### Adjust according to your need ABOVE
 ###############################################################################
@@ -37,7 +40,9 @@ CTRLDIR=$1          # assuming one is executing this script from the
 echo "DEBUG: setup environment"
 source ${CTRLDIR}/export_paths.ksh
 source ${BASE_CTRLDIR}/start_helper.sh
-source ${BASE_NAMEDIR}/loadenv_int2lm
+export INT2LM_BINDIR="${BASE_SRCDIR}/int2lm3.00"
+export INT2LM_EXNAME="int2lm3.00"
+source ${BASE_ENVSDIR}/loadenv_int2lm
 
 h0=$(date -u -d "$startDate" '+%H')
 d0=$(date -u -d "$startDate" '+%d')
@@ -45,7 +50,6 @@ m0=$(date -u -d "$startDate" '+%m')
 y0=$(date -u -d "$startDate" '+%Y')
 
 echo "DEBUG: def. individual settings"
-int2lm_nam_template="INT2LM_template_ERA5"
 
 echo "DEBUG: create INT2LM lm_cat_dir dir"
 int2lm_LmCatDir="${BASE_FORCINGDIR}/laf_lbfd/${y0}"
@@ -55,12 +59,12 @@ echo "DEBUG: create INT2LM rundir"
 rundir="${BASE_RUNDIR}/INT2LM_${y0}"
 mkdir -p ${rundir}
 
-###############################################################################
+################################################################################
 # Creating HISTORY.txt (reusability etc.)
-###############################################################################
+################################################################################
 histfile=${int2lm_LmCatDir}/HISTORY.txt
 echo "DEBUG: creating HISTORY.txt (reusability etc.)"
-cd ${BASE_BINDIR_INT2LM}
+cd ${INT2LM_BINDIR}
 TAG_INT2LM=$(git describe --tags)
 COMMIT_INT2LM=$(git log --pretty=format:'commit: %H' -n 1)
 AUTHOR_INT2LM=$(git log --pretty=format:'author: %an' -n 1)
@@ -103,7 +107,7 @@ EOM
 check4error $? "--- ERROR while creating HISTORY.txt"
 
 echo "DEBUG: copy INT2LM executable to rundir"
-cp ${BASE_BINDIR_INT2LM}/${BASE_INT2LM_EXNAME} ${rundir}/
+cp ${INT2LM_BINDIR}/${INT2LM_EXNAME} ${rundir}/
 
 echo "DEBUG start_date: ${start_date}"
 int2lm_start_date=${y0}${m0}${d0}${h0}
@@ -111,23 +115,26 @@ int2lm_start_date=${y0}${m0}${d0}${h0}
 echo "DEBUG: int2lm_start_date: ${int2lm_start_date}"
 
 echo "DEBUG: copy namelist for current loop"
-cp ${BASE_NAMEDIR}/${int2lm_nam_template} ${rundir}/INPUT
+cp ${BASE_NAMEDIR}/INT2LM/INPUT ${rundir}/INPUT
 
 echo "DEBUG: modify namelist (sed inserts etc.)"
 sed "s,__start_date__,${int2lm_start_date},g" -i ${rundir}/INPUT
 sed "s,__init_date__,${int2lm_start_date},g" -i ${rundir}/INPUT
 sed "s,__hstop__,${int2lm_hstop},g" -i ${rundir}/INPUT
 sed "s,__hincbound__,${int2lm_hincbound},g" -i ${rundir}/INPUT
-sed "s,__lm_ext_dir__,${BASE_GEODIR}/int2lm,g" -i ${rundir}/INPUT
-sed "s,__in_ext_dir__,${BASE_FORCINGDIR}/IrawIN/NT2LM_inext,g" -i ${rundir}/INPUT
-sed "s,__in_cat_dir__,${BASE_FORCINGDIR}/rawIN/${y0},g" -i ${rundir}/INPUT
+sed "s,__lm_ext_dir__,${BASE_GEODIR}/TSMP_EU11/static/int2lm,g" -i ${rundir}/INPUT
+sed "s,__in_ext_dir__,${BASE_FORCINGDIR}/ERA5raw/INT2LM_inext,g" -i ${rundir}/INPUT
+sed "s,__in_cat_dir__,${BASE_FORCINGDIR}/ERA5raw/${y0},g" -i ${rundir}/INPUT
 sed "s,__lm_cat_dir__,${int2lm_LmCatDir},g" -i ${rundir}/INPUT
+sed "s,__nprocx_int2lm__,${PROCX_INT2LM},g" -i ${rundir}/INPUT
+sed "s,__nprocy_int2lm__,${PROCY_INT2LM},g" -i ${rundir}/INPUT
+sed "s,__nprocio_int2lm__,${PROCIO_INT2LM},g" -i ${rundir}/INPUT
  
 echo "DEBUG: enter INT2LM rundir and start INT2LM"
 cd ${rundir}
 # just to be sure, clean rundir before starting (e.g. in case of restart)
 rm -rf YU*  
-srun ./${BASE_INT2LM_EXNAME}
+srun ./${INT2LM_EXNAME}
 # exit script, if something crashed int2lm
 if [[ $? != 0 ]] ; then exit 1 ; fi
 wait
