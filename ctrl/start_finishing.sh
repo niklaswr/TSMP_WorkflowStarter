f88923b3c8e7d228f5ccb7de2f80bd636f03b166
Niklas Wagner <n.wagner@fz-juelich.de>
2023-02-13 12:55:27 +0100

    Minor update to start_finishing.sh
    - add more loging
    - change used env-file
    - use new parallel gzip function
    - use parallel sha512sum funciton to speed things up

--- ctrl/start_finishing.sh
+++ ctrl/start_finishing.sh
@@ -17,11 +17,12 @@ echo "--- Simulation    init-date: ${initDate}"
 echo "---              start-data: ${startDate}"
 echo "---                  CaseID: ${CaseID}"
 echo "---            CaseCalendar: ${CaseCalendar}"
+echo "---             COMBINATION: ${COMBINATION}"
 echo "--- HOST:  $(hostname)"
 
 echo "--- source helper scripts"
 source ${BASE_CTRLDIR}/start_helper.sh
-source ${BASE_CTRLDIR}/postpro/loadenvs
+source ${BASE_CTRLDIR}/envs/env_finishing
 cd ${BASE_CTRLDIR}
 
 ###############################################################################
@@ -32,20 +33,23 @@ SimresDir=${BASE_SIMRESDIR}/${formattedStartDate}
 
 echo "--- gzip and sha512sum individual files in simresdir"
 cd ${SimresDir}/cosmo
-sha512sum ./* > CheckSum.sha512
-parallelGzip 48 ${SimresDir}/cosmo
+wrap_calc_sha512sum ${FIN_NTASKS} ./*
+parallelGzip ${FIN_NTASKS} ${SimresDir}/cosmo/*
 wait
 cd ${SimresDir}/parflow
-sha512sum ./* > CheckSum.sha512
-parallelGzip 48 ${SimresDir}/parflow
+wrap_calc_sha512sum ${FIN_NTASKS} ./*
+parallelGzip ${FIN_NTASKS} ${SimresDir}/parflow/*
 wait
 cd ${SimresDir}/clm
-sha512sum ./* > ./CheckSum.sha512
-parallelGzip 48 ${SimresDir}/clm
+wrap_calc_sha512sum ${FIN_NTASKS} ./*
+parallelGzip ${FIN_NTASKS} ${SimresDir}/clm/*
 wait
 cd ${SimresDir}/restarts
-sha512sum ./* > CheckSum.sha512
-parallelGzip 48 ${SimresDir}/restarts
+wrap_calc_sha512sum 1 ./*
+parallelGzip ${FIN_NTASKS} ${SimresDir}/restarts/*
+wait
+cd ${SimresDir}/log
+wrap_calc_sha512sum 1 ./*
 wait
 
 exit 0
